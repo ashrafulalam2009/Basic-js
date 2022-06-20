adfb7a466e9d289e2a1e8ff831b6846485b7a2c1
ashraful <ashrafulhuzhou@gmail.com>
2022-06-20 23:35:28 +0600

    basic js

---
+++ basic.js
@@ -0,0 +1,78 @@
+
+//variable
+
+var deposit = 400;
+// condition
+
+if(deposit > 500){
+
+}
+
+else if (deposit < 200 ){
+
+}
+
+else if(deposit == 500){
+
+}
+
+else if(deposit != 500){
+
+}
+
+else if (deposit >=500){
+
+
+
+}
+
+if(deposit <= 500){
+
+
+}
+
+else{
+
+}
+
+
+// array
+
+const numbers = [45,6587,45,1,365];
+
+const numberCount = number.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 555;
+
+// check whether 222 included in the array 
+
+if(numbers.indexOf(222) != -1){
+
+
+}
+
+
+if(numbers.includes(222)){
+
+
+}
+
+// loop
+// for , while
+for(const number of numbers){
+
+
+}
+
+// function 
+
+function fullName(first,second){
+    const name = first + '' + second ;
+
+}
+const person = fullName('Rahim','Ali');
+
+\\ Object
+
+const bottle = {color: 'yellow',contain:'water', price:'100'}
