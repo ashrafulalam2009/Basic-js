0c0f4dd7387a11740887e389801f8b917ae648db
ashraful <ashrafulhuzhou@gmail.com>
2022-07-02 00:00:08 +0600

    2 of 31 last part vvi

--- break-object.js
+++ break-object.js
@@ -1,17 +1,27 @@
-const fish = {id : 10, name:'hilsha', phone:'01754124',
- address: 'chandpur', dress: 'silver', price:100,};
+// const fish = {id : 10, name:'hilsha', phone:'01754124',
+//  address: 'chandpur', dress: 'silver', price:100,};
 
 // const phone = fish.phone;
 // const price = fish.price;
 // const address = fish.address;
 
-const{phone,price,address} = fish;
+// const{phone,price,address} = fish;
+
+const company = {name:'gp',ceo: {id:5, name:'ashraful',food:'fusca'}, web:{work:'webdevelopment',framework:'react'}}
 
 
+// const work = company.web.work;
+// const framework = company.web.framework;
+
+const {work,framework}= company.web;
+const {food}= company.ceo;
+
+// console.log(phone);
+// console.log(price);
+// console.log(address);
+// console.log(phone);
+// console.log(price);
+
+console.log(work,framework,food);
 
 
-console.log(phone);
-console.log(price);
-console.log(address);
-console.log(phone);
-console.log(price);
