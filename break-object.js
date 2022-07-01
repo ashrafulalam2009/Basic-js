8422b3e2932d2f920769eea91e94209925d55837
ashraful <ashrafulhuzhou@gmail.com>
2022-07-01 23:31:27 +0600

    2 of 31 first part

---
+++ break-object.js
@@ -0,0 +1,17 @@
+const fish = {id : 10, name:'hilsha', phone:'01754124',
+ address: 'chandpur', dress: 'silver', price:100,};
+
+// const phone = fish.phone;
+// const price = fish.price;
+// const address = fish.address;
+
+const{phone,price,address} = fish;
+
+
+
+
+console.log(phone);
+console.log(price);
+console.log(address);
+console.log(phone);
+console.log(price);
