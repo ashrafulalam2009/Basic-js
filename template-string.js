899f9670f189b94bc1f5418972e56162bcfdf76e
ashraful <ashrafulhuzhou@gmail.com>
2022-07-01 15:40:01 +0600

    some changes

--- template-string.js
+++ template-string.js
@@ -1,6 +1,6 @@
 const first = 'mamun';
 const second = 'chowdhory';
-const fullOld = 'first' + '' + 'second';
+const fullOld = 'this person name is' + first + '' + second;
 
 const bigName =` hello world 
 here is ashraful from Bangladesh.
@@ -8,7 +8,7 @@ afdssdfklsjffjf l fafsdf;jsdklfdf
 lsfjjfjdf
 `
 const fullNew = `This person name is: ${first} ${second}`;
-
+console.log(fullOld);
 console.log(fullNew);
 
 console.log(bigName);
