f36c9ff47dd9580fa7bc1ecb7165366c827a5632
ashraful <ashrafulhuzhou@gmail.com>
2022-07-01 09:37:39 +0600

    default parameter

---
+++ default-parameter.js
@@ -0,0 +1,21 @@
+function add(num1,num2 = 100){
+
+
+    // option1 
+    // num2 = num2 || 0;
+
+// option2
+// console.log(num1,num2);
+// if(num2 == undefined){
+//     num2 = 0;
+// }
+
+
+    const total = num1 + num2;
+    return total;
+
+}
+
+const result =add(15, 
+   5 );
+console.log(result);
