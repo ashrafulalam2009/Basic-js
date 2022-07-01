bd02abf3c05292a7514715c62240b065d0db9873
ashraful <ashrafulhuzhou@gmail.com>
2022-07-01 15:26:06 +0600

    multiple line string

--- template-string.js
+++ template-string.js
@@ -2,6 +2,13 @@ const first = 'mamun';
 const second = 'chowdhory';
 const fullOld = 'first' + '' + 'second';
 
-const fullNew = `${first} ${second}`;
+const bigName =` hello world 
+here is ashraful from Bangladesh.
+afdssdfklsjffjf l fafsdf;jsdklfdf
+lsfjjfjdf
+`
+const fullNew = `This person name is: ${first} ${second}`;
 
 console.log(fullNew);
+
+console.log(bigName);
