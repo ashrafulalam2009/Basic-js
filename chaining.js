86d17cdf3c05e904a39c4efb1c83d98dfeb7b1a5
ashraful <ashrafulhuzhou@gmail.com>
2022-07-02 23:24:00 +0600

    destructing array

--- chaining.js
+++ chaining.js
@@ -2,5 +2,18 @@
 
 const myObject = {x:2, y:50,z : 600, a:25, b:68};
 
-const{x} = myObject;
-console.log(x);
+const{x,b} = myObject;
+console.log(b);
+
+// destructuring array
+
+const [p,q] = [35,50, 91,96];
+
+console.log(p,q);
+
+const [best,good] = ['mohona','nipa'];
+console.log(best,good);
+
+const{sky,soil,money,color} = {sky:'blue',soil:'soft',color:'red',money:500}
+
+console.log(sky,color,money);
