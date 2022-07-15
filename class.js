6e8e43f41730da3d517f13fd4d8f856ce945cd8d
ashraful <ashrafulhuzhou@gmail.com>
2022-07-15 17:47:05 +0600

    car class

---
+++ class.js
@@ -0,0 +1,23 @@
+
+
+//  class ClassName{
+
+//     constructor(){}
+
+//  }
+
+
+class Car {
+    constructor(name,year) {
+    this.name = name;
+    this.year = year;
+
+    }
+}
+
+const car1 = new Car("Toyota", 2017);
+const car2 = new Car("suzuki", 2020);
+
+console.log(car1);
+console.log(car2);
+
