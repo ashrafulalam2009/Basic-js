424acc7602f0f4d04eb4c0ebae944b91d6b12883
ashraful <ashrafulhuzhou@gmail.com>
2022-07-15 17:53:11 +0600

    class of person

--- class.js
+++ class.js
@@ -18,6 +18,21 @@ class Car {
 const car1 = new Car("Toyota", 2017);
 const car2 = new Car("suzuki", 2020);
 
-console.log(car1);
-console.log(car2);
+// console.log(car1);
+// console.log(car2);
+
+
+class Person {
+    constructor(name,age) {
+    this.name = name;
+    this.age = age;
+
+    }
+}
+
+const person1 = new Person("sakib",35 );
+const person2 = new Person("tamim", 38);
+
+console.log(person1);
+console.log(person2);
 
